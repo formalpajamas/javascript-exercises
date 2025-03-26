885df4ccb20b1deb26420c9293fcfb8496d2195d
Matthew Netherby <202042058+formalpajamas@users.noreply.github.com>
2025-03-26 11:43:21 -0600

    Edit reverseString.js to reverse user input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+// get length of string and loop that amount of time, each time taking the next letter
+const reverseString = function(string) {
+    let newString = "";
+    let i = string.length;
+    while (i > 0) {
+        newString += string[i-1];
+        i--;
+    }
+    return newString;
 };
 
 // Do not edit below this line
