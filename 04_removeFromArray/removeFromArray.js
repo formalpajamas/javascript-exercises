0660ea3d46fb2c442dda47e6d716fa65e4e830fc
Matthew Netherby <202042058+formalpajamas@users.noreply.github.com>
2025-03-26 20:31:50 -0600

    Edit removeFromArray.js and spec.js file
    Created function to remove matching elements that
    users input in the removeFromArray function.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...item) {
+    item.forEach((item) => {
+        let i = 0;
+        while (i < array.length) {
+            if (array[i] === item) {
+                array.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
