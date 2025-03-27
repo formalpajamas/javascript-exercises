6edd62d9fd301fa164b53f8f9cf91624314d2f53
Matthew Netherby <202042058+formalpajamas@users.noreply.github.com>
2025-03-26 21:20:31 -0600

    Edit tempConversion.js and spec files
    Create two functions to convert Celsius or
    Fahrenheit to the other and return a number
    rounded to one decimal place.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celsius = ((temp - 32) * (5/9));
+  if (Number.isInteger(celsius)) {
+    return celsius;
+  } else {
+    return parseFloat(celsius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahrenheit = (temp * (9/5) + 32);
+  if (Number.isInteger(fahrenheit)) {
+    return fahrenheit;
+  } else {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
