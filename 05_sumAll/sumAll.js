4eb4bfc0727ffc8cfe689022700b13249cf39eae
Matthew Netherby <202042058+formalpajamas@users.noreply.github.com>
2025-03-26 20:56:49 -0600

    Edit sumAll.js and spec files
    Create a sumAll function that adds all the numbers
    between two user inputs.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let finalSum = 0;
+    if (
+        int1 > 0 && int2 > 0 &&
+        Number.isInteger(int1) && Number.isInteger(int2)) {
+            if (int1 < int2) {
+                let i = int1;
+                while (i <= int2) {
+                    finalSum += i;
+                    i++;
+                }
+            } else {
+                let i = int2;
+                while (i <= int1) {
+                    finalSum += i;
+                    i++;
+                }
+            } 
+            return finalSum;
+        } else {
+            return "ERROR";
+        }
 };
 
 // Do not edit below this line
